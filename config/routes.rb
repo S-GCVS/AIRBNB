18b6b2a8b9f51c44c6f8a55c27645d3a4866fdca
Suzanne Goncalves <suzanne.g.p@hotmail.fr>
2023-03-27 23:47:48 +0200

    Modification routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "houses#index"
   resources :houses, only: %i[index new show create]
 
   # Defines the root path route ("/")
